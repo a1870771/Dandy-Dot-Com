05aae10e79c7a29fb26f7b6e287a5bfe64ee9634
Jack <jackrlarkin2004@gmail.com>
2023-08-14 10:41:36 +0930

    forgor

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import Head from 'next/head'
-import Nav from '@/components/Nav'
+import Landing from '@/components/Landing'
 export default function Home() {
   return (
   <>
     <Head>
       <title>home | The Dandy Buzzkills</title>
     </Head>
-    <Nav/>
-
+    <div className="bg-white">
+      <Landing/>
+  </div>
   </>
   )
 }
