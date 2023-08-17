0ca130461704ce331859eacb3f4f7b9c92f9e1f6
Jack <jackrlarkin2004@gmail.com>
2023-08-17 20:19:23 +0930

    fix z index on dropdown

--- src/pages/epk/media.js
+++ src/pages/epk/media.js
@@ -14,7 +14,7 @@ export default function Home() {
     </Head>
     <div className='container mx-auto px-5'>
       <Header />
-      <h1 className='text-5xl text-center py-16 font-bold'>Photos</h1>
+      <h1 className='text-5xl text-center pb-8 -pt-8 md:py-16 font-bold'>Photos</h1>
       <div className="max-w-7xl mx-auto pb-16 px-4 sm:pb-24 sm:px-6 max-w-[75vw] lg:px-8">
         <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
