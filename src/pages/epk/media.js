ad0b35c98c39ee67e182c6b533e44d3fac04f9f6
Jack Larkin <a1870771@adelaide.edu.au>
2023-08-30 10:41:54 +0930

    photo subheading

--- src/pages/epk/media.js
+++ src/pages/epk/media.js
@@ -14,7 +14,8 @@ export default function Home() {
     </Head>
     <div className='container mx-auto px-5'>
       <Header />
-      <h1 className='text-5xl text-center pb-8 -pt-8 md:py-16 font-bold'>Photos</h1>
+      <h1 className='text-5xl text-center pb-8 -pt-8 md:pt-16 md:pb-4 font-bold'>Photos</h1>
+      <p className='text-center pb-8 text-gray-600'>(Open in new tab for full resolution)</p>
       <div className="max-w-7xl mx-auto pb-16 px-4 sm:pb-24 sm:px-6 max-w-[75vw] lg:px-8">
         <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
